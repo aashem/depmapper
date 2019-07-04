bde07644674808fcf58cd012a8b913174e9388ab
aashem <shemeikka.asla@gmail.com>
2019-07-04 13:27:55 +0300

    fix typo & port

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 //what database to use
 let MONGOURI = process.env.MONGO_URI || 'mongodb://localhost:27017/depmapper'
 
-//Process.env.PORT is specific port chosen in ENV , 27017 = default mongodb port
-let PORT = process.env.PORT || 3001
+
+let PORT = process.env.PORT
 
 module.exports = {
     MONGOURI, 
