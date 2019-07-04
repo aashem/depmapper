cc496a37ae53ece95280c200f5e892e54b580fc1
aashem <shemeikka.asla@gmail.com>
2019-07-04 13:19:20 +0300

    removed dependencies

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
-let MONGOURI = process.env.MONGO_URI
-let PORT = process.env.PORT
+//what database to use
+let MONGOURI = process.env.MONGO_URI || 'mongodb://localhost:27017/depmapper'
+
+//Process.env.PORT is specific port chosen in ENV , 27017 = default mongodb port
+let PORT = process.env.PORT || 3001
 
 module.exports = {
     MONGOURI, 
